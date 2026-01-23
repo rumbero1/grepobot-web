3ce170f50e7ed2a9c878b74caf9345e2a179569d
rumbero1 <irc_sal@hotmail.com>
2026-01-23 23:26:53 +0100

    Create eslint.config.js file

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+};
