410aff9a92a12caf75f4a729d03aca1dbc81407c
rumbero1 <irc_sal@hotmail.com>
2026-01-24 10:17:04 +0100

    Remove tailwind.config.ts file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,1 @@
-/**
- * @type {import('tailwindcss').Config}
- */
 
-module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    './public/index.html',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
