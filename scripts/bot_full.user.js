fc5ae92e78ef98288286d29944f5b224511ffc20
rumbero1 <irc_sal@hotmail.com>
2026-01-25 15:10:02 +0100

    Add GrepoBot user script for testing purposes

---
+++ scripts/bot_full.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         GrepoBot - Full (prueba)
+// @namespace    https://grepobot.example/
+// @version      0.0.1
+// @description  Script de prueba para verificar /api/descargar en despliegue
+// @author       prueba
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  console.log("GrepoBot prueba: script descargado y ejecutado.");
+})();
